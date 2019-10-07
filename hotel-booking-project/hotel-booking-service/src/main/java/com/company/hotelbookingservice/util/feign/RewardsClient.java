caf048db8144ae25b33e573f3f7a443ba18f0c74
Akshay Choudhary <ak92choudhary@gmail.com>
2019-10-07 15:12:18 -0400

    made the feign clients

---
+++ hotel-booking-project/hotel-booking-service/src/main/java/com/company/hotelbookingservice/util/feign/RewardsClient.java
@@ -0,0 +1,17 @@
+package com.company.hotelbookingservice.util.feign;
+
+
+import com.company.hotelbookingservice.model.Rewards;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(name = "rewards-service")
+public interface RewardsClient {
+    @GetMapping(value = "/rewards/type/{roomType}")
+    Rewards getrewards();
+
+    @GetMapping(value = "/rewards")
+    List<Rewards> getAllRewards(); 
+}
